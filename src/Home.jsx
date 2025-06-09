4bb3161b4dbc169d99539ad6226428e306a3f419
se22uari036 <se22uari036@mahindrauniversity.edu.in>
2025-06-09 15:42:16 +0530

    thada

---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Home() {
+    const products = [
+        { 
+            id:1, 
+            name:"Product 1" ,
+            desc:"thios ygygugiu yuyuu uiuihbk", 
+            imgUrl:""
+        }
+    ]
+
+    let a = "Cherith"
+
+  return (
+    <div>Home
+        <h1>hello World!</h1>
+        <p> welcome {a}.</p>
+    </div>
+  )
+}
---
+++ src/Register.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+export default function Register() {
+  const [user, setUser] = useState({});
+  const [count, setCount] = useState(0);
+  const [val, setVal] = useState(0);
+  const handleClick = () => {
+    alert("Hello World");
+  };
+  const updateCount = () => {
+    setCount(count + 1);
+  };
+
+   const add = () => {
+    setVal(val + 1);
+  };
+    const sub = () => {
+    setVal(val - 1);
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <p>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+      </p>
+      <p>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Enter Email Address"
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          onChange={(e) => setUser({ ...user, pass: e.target.value })}
+          placeholder="New Password"
+        />
+      </p>
+      <p>
+        <button>Submit</button>
+      </p>
+      <hr />
+      <p>
+        <Link to="/login">Aready a member? Login Here...</Link>
+      </p>
+      <hr />
+      <button onClick={handleClick}>Click</button>
+      <hr />
+      <p>
+        {count}<br></br>
+        <button onClick={updateCount}>Update Count</button>
+      </p>
+      
+       <p>
+        <button onClick={sub}>-</button> 
+        {val} 
+        <button onClick={add}>+</button>
+      </p>
+    </div>
+  );
+}
